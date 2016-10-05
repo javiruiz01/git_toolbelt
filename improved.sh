36bb0ebaad0477499b49d80fd5eb3d04d82fd97f
Javier Ruiz Calle <jarc0212@gmail.com>
2016-10-05 10:30:05 +0200

    Colored help and now accept path to repository

--- improved.sh
+++ improved.sh
@@ -14,11 +14,11 @@ function commit {
 function upstream {
 	echo "Refreshing the UPSTREAM"
 	git fetch upstream
-	if [ $? -eq 0 ]
+	if [[ $? -eq 0 ]]
 	then
 		echo "GIT CHECKOUT MASTER"
 		git checkout master
-		if [ $? -eq 0 ]
+		if [[ $? -eq 0 ]]
 		then
 			echo "GIT MERGE UPSTREAM/MASTER"
 			git merge upstream/master
@@ -33,34 +33,62 @@ function pusheen {
 	return
 }
 
-if [ $# -eq 0 ]
-then
-	echo "Usage: `basename $0` options"
-	echo "If you don't want to use the options,
-please use the refresh.sh scrip."
-	exit 2
-fi
+function print_usage() {
+	printf "\e[31mUsage:\e[0m `basename $0` [OPTIONS]\n"
+	printf "\e[32mOPTIONS:\e[0m\n"
+	printf "	-p		Push to master\n"
+	printf "	-c 		Commit\n"
+	printf "	-u		Refresh upstream\n"
+	printf "			Must already be set\n"
+	printf "	-h 		Display HELP\n"
+	printf "	-f path		Add path to repository\n"
+	printf "\e[32mEXAMPLE:\e[0m\n"
+	printf "	$0 -f /path/to/file -c\n"
+	printf "\e[7m|------------------------------------------------|\e[0m\n"
+	echo "If you don't want to use the options,"
+	echo "please use the refresh.sh script"
+	return
+}
 
-while getopts ":pcu" opt
-do
-	case $opt in
-		c)
-			echo "I should do a commit"
-			commit
-			;;
-		\?)
-			echo "Invalid option: -$OPTARG"
-			;;
-		p)
-			echo "I should do a PUSHEEN"
-			pusheen
-			;;
-		u)
-			echo "I should update the UPSTREAM"
-			upstream
-			;;
-	esac
-done
+function main {
+	echo "$@"
+	if [[ $# -eq 0 ]]
+	then
+		print_usage
+	fi
+
+	while getopts ":pcuhf:" opt
+	do
+		case $opt in
+			c)
+				echo "I should do a commit"
+				commit
+				;;
+			\?)
+				echo "Unknown option: -$OPTARG"
+				;;
+			p)
+				echo "I should do a PUSHEEN"
+				pusheen
+				;;
+			u)
+				echo "I should update the UPSTREAM"
+				upstream
+				;;
+			h)
+				echo "I should go help you"
+				print_usage
+				;;
+			f)	echo "Following PATH"
+				my_path=$OPTARG
+				cd $my_path
+				echo `pwd`		
+				;;
+		esac
+	done
+}
+
+main "$@"
 
 echo "Exiting"
 exit 0
